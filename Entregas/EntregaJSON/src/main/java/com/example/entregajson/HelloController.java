5300736c1034c943e7710fb7d2ce91c0aac74ddf
Andrés López Zamarreño <lopez.zama@gmail.com>
2022-11-27 23:12:12 +0100

    update

--- Entregas/EntregaJSON/src/main/java/com/example/entregajson/HelloController.java
+++ Entregas/EntregaJSON/src/main/java/com/example/entregajson/HelloController.java
@@ -9,7 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,15 +18,14 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 import org.json.JSONObject;
 
-public class HelloController implements Initializable {
+public class HelloController implements Initializable, EventHandler<ActionEvent> {
     @FXML
-    private RadioButton radioEdad,radioNacionalidad,radioGenero;
+    private RadioButton radioEdad, radioNacionalidad, radioGenero;
+    @FXML
+    private MenuItem buscarEdad, buscarNacionalidad, buscarGenero;
 
     @FXML
     private Button botonBuscar;
@@ -36,7 +35,8 @@ public class HelloController implements Initializable {
 
     private ObservableList<String> cosas;
 
-    private String url,xml,textoVuelta;
+    private String url, xml, textoVuelta;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         instancias();
@@ -44,64 +44,128 @@ public class HelloController implements Initializable {
     }
 
     private void acciones() {
-        botonBuscar.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                RadioButton radio = (RadioButton)grupoRadios.getSelectedToggle();
-                String seleccionado = radio.getText();
-                System.out.println(seleccionado);
-                if(seleccionado.equalsIgnoreCase("Edad")){
-                    url =  "https://api.agify.io/?name=";
-                    xml = "edad-view.fxml";
-                } else if (seleccionado.equalsIgnoreCase("Genero")) {
-                    url = "https://api.genderize.io?name=";
-                    xml = "genero-view.fxml";
-                } else if (seleccionado.equalsIgnoreCase("Nacionalidad")) {
-                    url = "https://api.nationalize.io/?name=";
-                    xml = "nacionalidad-view.fxml";
-                }
-                Stage ventana = new Stage();
-                //2.  FXML --->parte grafica
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(xml));
-                //3. Tener una scene
-                try {
-                    Parent root = fxmlLoader.load();
-                    Scene escena = new Scene(root,500,400);
-                    //4. unir scene y stage
-                    ventana.setScene(escena);
-
-                    if(seleccionado.equalsIgnoreCase("Edad")){
-                        EdadController controller = fxmlLoader.getController();
-                        controller.setController(HelloController.this);
-                        controller.setUrlRecuperada(url);
-                    } else if (seleccionado.equalsIgnoreCase("Genero")) {
-                        GeneroController controller = fxmlLoader.getController();
-                        controller.setController(HelloController.this);
-                        controller.setUrlRecuperada(url);
-                    } else if (seleccionado.equalsIgnoreCase("Nacionalidad")) {
-                        NacionalidadController controller = fxmlLoader.getController();
-                        controller.setController(HelloController.this);
-                        controller.setUrlRecuperada(url);
-                    }
-
-                    //5. hacer visible la ventana
-                    ventana.setTitle("Ventana secundaria");
-                    ventana.show();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        botonBuscar.setOnAction(this);
+        buscarEdad.setOnAction(this);
+        buscarNacionalidad.setOnAction(this);
+        buscarGenero.setOnAction(this);
     }
 
     private void instancias() {
         grupoRadios = new ToggleGroup();
-        grupoRadios.getToggles().addAll(radioEdad,radioNacionalidad,radioGenero);
+        grupoRadios.getToggles().addAll(radioEdad, radioNacionalidad, radioGenero);
         cosas = FXCollections.observableArrayList();
     }
 
-    public void recepcionarText(String texto){
+    public void recepcionarText(String texto) {
         cosas.add(texto);
         listaCosas.setItems(cosas);
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == botonBuscar) {
+            RadioButton radio = (RadioButton) grupoRadios.getSelectedToggle();
+            String seleccionado = radio.getText();
+            System.out.println(seleccionado);
+            if (seleccionado.equalsIgnoreCase("Edad")) {
+                url = "https://api.agify.io/?name=";
+                xml = "edad-view.fxml";
+            } else if (seleccionado.equalsIgnoreCase("Genero")) {
+                url = "https://api.genderize.io?name=";
+                xml = "genero-view.fxml";
+            } else if (seleccionado.equalsIgnoreCase("Nacionalidad")) {
+                url = "https://api.nationalize.io/?name=";
+                xml = "nacionalidad-view.fxml";
+            }
+            Stage ventana = new Stage();
+            //2.  FXML --->parte grafica
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(xml));
+            //3. Tener una scene
+            try {
+                Parent root = fxmlLoader.load();
+                Scene escena = new Scene(root, 500, 400);
+                //4. unir scene y stage
+                ventana.setScene(escena);
+
+                if (seleccionado.equalsIgnoreCase("Edad")) {
+                    EdadController controller = fxmlLoader.getController();
+                    controller.setController(HelloController.this);
+                    controller.setUrlRecuperada(url);
+                } else if (seleccionado.equalsIgnoreCase("Genero")) {
+                    GeneroController controller = fxmlLoader.getController();
+                    controller.setController(HelloController.this);
+                    controller.setUrlRecuperada(url);
+                } else if (seleccionado.equalsIgnoreCase("Nacionalidad")) {
+                    NacionalidadController controller = fxmlLoader.getController();
+                    controller.setController(HelloController.this);
+                    controller.setUrlRecuperada(url);
+                }
+
+                //5. hacer visible la ventana
+                ventana.setTitle("Ventana secundaria");
+                ventana.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (actionEvent.getSource() == buscarEdad) {
+            Stage ventana = new Stage();
+            //2.  FXML --->parte grafica
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("edad-view.fxml"));
+            //3. Tener una scene
+            try {
+                Parent root = fxmlLoader.load();
+                Scene escena = new Scene(root, 500, 400);
+                //4. unir scene y stage
+                ventana.setScene(escena);
+                EdadController controller = fxmlLoader.getController();
+                controller.setController(HelloController.this);
+                controller.setUrlRecuperada("https://api.agify.io/?name=");
+                //5. hacer visible la ventana
+                ventana.setTitle("Ventana secundaria");
+                ventana.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (actionEvent.getSource() == buscarGenero) {
+            Stage ventana = new Stage();
+            //2.  FXML --->parte grafica
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("genero-view.fxml"));
+            //3. Tener una scene
+            try {
+                Parent root = fxmlLoader.load();
+                Scene escena = new Scene(root, 500, 400);
+                //4. unir scene y stage
+                ventana.setScene(escena);
+                GeneroController controller = fxmlLoader.getController();
+                controller.setController(HelloController.this);
+                controller.setUrlRecuperada("https://api.genderize.io?name=");
+                //5. hacer visible la ventana
+                ventana.setTitle("Ventana secundaria");
+                ventana.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else if (actionEvent.getSource() == buscarNacionalidad) {
+            Stage ventana = new Stage();
+            //2.  FXML --->parte grafica
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("nacionalidad-view.fxml"));
+            //3. Tener una scene
+            try {
+                Parent root = fxmlLoader.load();
+                Scene escena = new Scene(root, 500, 400);
+                //4. unir scene y stage
+                ventana.setScene(escena);
+                NacionalidadController controller = fxmlLoader.getController();
+                controller.setController(HelloController.this);
+                controller.setUrlRecuperada("https://api.nationalize.io/?name=");
+                //5. hacer visible la ventana
+                ventana.setTitle("Ventana secundaria");
+                ventana.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 }
